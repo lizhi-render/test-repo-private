afafec4963ee3a251428f255e3e99ae75488c8b7
lizhi-render <lizhi@render.com>
2025-05-11 22:39:19 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World Changing this alone does nothing.{" "}
+      Hello World.{" "}
       <Link href="/about">
         About
       </Link>
