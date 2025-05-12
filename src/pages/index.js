1a6a94c4b2433a341c67fd8a796f033f16a46b3e
lizhi-render <lizhi@render.com>
2025-05-11 22:33:17 -0400

    Should not deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Hello World Changing this alone does nothing.{" "}
       <Link href="/about">
         About
       </Link>
